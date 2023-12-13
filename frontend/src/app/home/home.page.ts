5a72f2f1dc9c2ca692373cce7bdbe559588451c7
VictorVCz <vvelcruz@gmail.com>
2023-12-13 15:58:48 -0300

    commit final

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ModalController } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  posts: any[] = [];
+  search: string = "";
+  
 
+  constructor(private http: HttpClient, private modalController: ModalController) {}
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData() {
+    
+    const apiUrl = 'http://localhost:3000/posts'; // Reemplaza esto con tu URL y endpoint
+    this.http.get(apiUrl).subscribe((data: any) => {
+      this.posts = data;
+    });
+  }
+
+  deletePost(id: number) {
+    
+    
+    const apiUrl = `http://localhost:3000/posts/${id}`; // Reemplaza esto con tu URL y endpoint
+    this.http.delete(apiUrl).subscribe(() => {
+      // Eliminación exitosa, actualiza la lista de posts
+      
+      this.getData();
+    });
+  }
+  
+  
+  
 }
